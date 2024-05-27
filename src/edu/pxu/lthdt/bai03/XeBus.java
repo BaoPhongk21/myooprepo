6e1733a31510892015eb3ecdf8b1067fb0c37cf6
Lenovo <Lenovo@DESKTOP-2TK5V4G>
2024-05-27 23:28:55 +0700

    hoan thanh tat ca bai tap

--- src/edu/pxu/lthdt/bai03/XeBus.java
+++ src/edu/pxu/lthdt/bai03/XeBus.java
@@ -1,79 +1,74 @@
 package edu.pxu.lthdt.bai03;
 
+import java.time.LocalDate;
+
 public class XeBus {
-    String hangxe;
-    int namsanxuat;
-    int dongco;
-    int soghe;
-
-    public XeBus(String hangxe, int namsanxuat, int dongco, int soghe) {
-        this.hangxe = hangxe;
-        this.setNamsanxuat(namsanxuat);
-        this.dongco = dongco;
-        this.soghe = soghe;
-    }
-
-    public String getHangxe() {
-        return hangxe;
-    }
-
-    public void setHangxe(String hangxe) {
-        this.hangxe = hangxe;
-    }
-
-    public int getNamsanxuat() {
-        return namsanxuat;
-    }
-
-    public void setNamsanxuat(int namsanxuat) {
-        if(namsanxuat<1829 || namsanxuat>2024){
-            System.out.println("nam san xuat khong hop le");
-        }else {
-            this.namsanxuat = namsanxuat;
-        }      
-    }
-
-    public int getDongco() {
-        return dongco;
-    }
-
-    public void setDongco(int dongco) {
-        if(dongco<150 || dongco>800){
-            System.out.println("khong co lua chon phu hop");
-        }else{
-            this.dongco = dongco;
-        }        
-    }
-
-    public int getSoghe() {
-        return soghe;
-        
-    }
-
-    public void setSoghe(int soghe) {
-        if(soghe<8 || soghe>104){
-            System.out.println("so ghe khong hop le");
-        }else {
-             this.soghe = soghe;
-        }
-       
-    }
-
-    @Override
-    public String toString() {
-        if(namsanxuat==0 || dongco==0 || soghe==0){
-            return "Xe Bus{" + "hang xe=" + hangxe + '}'; 
-        }else if(namsanxuat==0){
-            return "Xe Bus{" + "hang xe=" + hangxe + ", dong co=" + dongco + ", so ghe=" + soghe + '}';
-        }else if (dongco==0){
-            return "Xe Bus{" + "hang xe=" + hangxe + ", nam san xuat=" + namsanxuat + ", so ghe=" + soghe + '}';
-        }
-        else if(soghe==0){
-            return "Xe Bus{" + "hang xe=" + hangxe + ", nam san xuat=" + namsanxuat + ", dong co=" + dongco + '}';
-        }else {
-            return "Xe Bus{" + "hang xe=" + hangxe + ", nam san xuat=" + namsanxuat + ", dong co=" + dongco + ", so ghe=" + soghe + '}';
-        }
-        
-    }
+	private String hangxe;
+	private int namsanxuat;
+	private float dongco;
+	private int soghe;
+	
+	public XeBus(String hangxe, int namsanxuat, float dongco, int soghe) {
+		super();
+		this.setHangxe(hangxe);
+		this.setNamsanxuat(namsanxuat);
+		this.setDongco(dongco);
+		this.setSoghe(soghe);
+	}
+	
+	public String getHangxe() {
+		return hangxe;
+	}
+	
+	public void setHangxe(String hangxe) {
+		if ("Hanoibus (Transerco)".equals(hangxe) || "Thaco Bus".equals(hangxe) || "SAMCO".equals(hangxe) || "Vinamoto".equals(hangxe) || "DCAR".equals(hangxe)) {
+			this.hangxe = hangxe;
+		} else {
+	        System.out.println("Hãng xe " + hangxe + " không được sản xuất tại Việt Nam.");
+	    }
+	}
+	
+	public int getNamsanxuat() {
+		return namsanxuat;
+	}
+	
+	public void setNamsanxuat(int namsanxuat) {
+		LocalDate ngayht = LocalDate.now();
+		int namhientai = ngayht.getYear();
+		if((namsanxuat < 1894) || (namsanxuat > namhientai)){
+			System.out.println("Năm sản xuất không phù hợp");
+		}else {
+			this.namsanxuat = namsanxuat;
+		}
+	}
+	
+	public float getDongco() {
+		return dongco;
+	}
+	
+	public void setDongco(float dongco) {
+		if (dongco <= 0) {
+            System.out.println("Động cơ không thể có công xuất là: " + dongco + "kW");
+		}else {
+			this.dongco = dongco;
+		}
+	}
+	
+	public int getSoghe() {
+		return soghe;
+	}
+	
+	public void setSoghe(int soghe) {
+		if ((soghe <=36) && (soghe >=60)) {
+			System.out.println("Số ghế: "+soghe+" không hợp lệ");
+		}
+		this.soghe = soghe;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "XeBus [hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + ", soghe=" + soghe	+ "]";
+	}
 	
 }
--- src/edu/pxu/lthdt/bai03/XeOto.java
+++ src/edu/pxu/lthdt/bai03/XeOto.java
@@ -1,4 +1,5 @@
 package edu.pxu.lthdt.bai03;
+
 import java.time.LocalDate;
 
 public class XeOto {
@@ -6,65 +7,53 @@ public class XeOto {
 	private int namsanxuat;
 	private float dongco;
 	
-	
-	
 	public XeOto(String hangxe, int namsanxuat, float dongco) {
 		super();
 		this.setHangxe(hangxe);
 		this.setNamsanxuat(namsanxuat);
 		this.setDongco(dongco);
 	}
-
-
-
+	
+	
 	public String getHangxe() {
 		return hangxe;
 	}
-
-
-
 	public void setHangxe(String hangxe) {
-		this.hangxe = hangxe;
+		if ("Toyota".equals(hangxe) || "Chevrolet".equals(hangxe) || "Ford".equals(hangxe) || "Honda".equals(hangxe) || "Hyundai".equals(hangxe)) {
+			this.hangxe = hangxe;
+		} else {
+	        System.out.println("Hãng xe " + hangxe + " không được sản xuất tại Việt Nam.");
+	    }
 	}
-
-
-
+	
 	public int getNamsanxuat() {
 		return namsanxuat;
 	}
-
-
-
 	public void setNamsanxuat(int namsanxuat) {
-		java.time.LocalDate ngayht = LocalDate.now();
+		LocalDate ngayht = LocalDate.now();
 		int namhientai = ngayht.getYear();
-		if((namsanxuat < 1885) || (namsanxuat > namhientai)) {
-			System.out.print("Nam san xuat khong phu hop");
+		if((namsanxuat < 1885) || (namsanxuat > namhientai)){
+			System.out.println("Năm sản xuất không hợp lệ");
+		}else {
+			this.namsanxuat = namsanxuat;
 		}
-		else {
-		this.namsanxuat = namsanxuat;
 	}
-	}
-
-
+	
 	public float getDongco() {
 		return dongco;
 	}
-
-
-
 	public void setDongco(float dongco) {
-		this.dongco = dongco;
+		if (dongco <= 0) {
+            System.out.println("Động cơ không thể có công xuất là: " + dongco + "kW");
+		}else {
+			this.dongco = dongco;
+		}
 	}
-
-
-
+	
+	
 	@Override
 	public String toString() {
 		return "XeOto [hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + "]";
 	}
-	
-	
-	
 	
 }
